bff453d330dc70451a9e4d6b4c827a51085ed1cb
manuk257 <manu.manuk1873@gmail.com>
2024-10-16 12:40:25 +0530

    first commit

--- src/tests/crud/test_crud_better.py
+++ src/tests/crud/test_crud_better.py
@@ -1,51 +1,49 @@
-# API TestCase
+# Conftest
 
-# URL -> API_Constants.py
-# headers -> Utils.py
-# payload -> payload_manager.py
-# HTTP POST -> api_request_wrapper.py
-# verification -> common_verification.py
+# Create Token
+# Create Booking Id
+# Update the Booking(Put) - BookingID, Token
+# Delete the Booking
+
+# Verify that created booking id when we update we are able to update it and delete it also
 
 import allure
 import pytest
-from src.helpers.api_requests_wrapper import post_request
+
 from src.constants.api_constants import APIConstants
-from src.helpers.payload_manager import payload_create_booking
+from src.helpers.api_requests_wrapper import *
 from src.helpers.common_verification import *
+from src.helpers.payload_manager import *
 from src.utils.utils import Utils
-import logging
 
-class TestCreateBooking():
-    @pytest.mark.positive
-    @allure.title("Verify that Create Booking Status and Booking ID shouldn't be null")
+
+class TestCRUDBooking(object):
+    @pytest.mark.put
+    @allure.title("Test CRUD operation Update(PUT).")
     @allure.description(
-        "Creating a Booking from the paylaod and verfiy that booking id should not be null and status code should be 200 for the correct payload")
-    def test_create_booking_positive(self):
-        LOGGER = logging.getLogger(__name__)
-        LOGGER.info("Starting the Testcase - TC1 - positive")
-        response = post_request(
-            url=APIConstants().url_create_booking(),
+        "Verify that Full Update with the booking ID and Token is working.")
+    def test_update_booking_id_token(self, create_token, get_booking_id):
+        put_url = APIConstants.url_patch_put_delete(booking_id=get_booking_id)
+        print(put_url)
+        response = put_requests(
+            url=put_url,
+            headers=Utils().common_header_put_delete_patch_cookie(token=create_token),
+            payload=payload_update_booking(),
             auth=None,
-            headers=Utils().common_headers_json(),
-            payload=payload_create_booking(),
             in_json=False
         )
+        # Verification here & more
+        verify_response_key(response.json()["firstname"], "Amit")
+        verify_response_key(response.json()["lastname"], "Brown")
         verify_http_status_code(response_data=response, expected_data=200)
-        verify_json_key_for_not_null(response.json()["bookingid"])
-        LOGGER.info(response.json()["bookingid"])
-        LOGGER.info("End of the Testcase TC1 -positive")
 
-    @pytest.mark.negative
-    @allure.title("Verify that Create Booking doesn't work with no payload")
-    @allure.description(
-        "Creating a Booking with no payload and verify that booking id")
-    def test_create_booking_negative(self):
-        LOGGER = logging.getLogger(__name__)
-        response = post_request(
-            url=APIConstants().url_create_booking(),
+    def test_delete_booking_id(self, create_token, get_booking_id):
+        delete_url = APIConstants.url_patch_put_delete(booking_id=get_booking_id)
+        response = delete_requests(
+            url=delete_url,
+            headers=Utils().common_header_put_delete_patch_cookie(token=create_token),
             auth=None,
-            headers=Utils().common_headers_json(),
-            payload={},
             in_json=False
         )
-        verify_http_status_code(response_data=response, expected_data=500)
+        verify_response_delete(response=response.text)
+        verify_http_status_code(response_data=response, expected_data=201)
